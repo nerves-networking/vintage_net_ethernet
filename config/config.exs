aa9534e78c1d056d3fb174f32619addf25c522a5
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2020-07-22 15:39:35 -0700

    Remove bin path references

--- config/config.exs
+++ config/config.exs
@@ -5,9 +5,7 @@ use Mix.Config
 # * udhcpc_handler: capture whatever happens with udhcpc
 # * resolvconf: don't update the real resolv.conf
 # * persistence_dir: use the current directory
-# * bin_ip: just fail if anything calls ip rather that run it
 config :vintage_net,
   udhcpc_handler: VintageNetTest.CapturingUdhcpcHandler,
   resolvconf: "/dev/null",
-  persistence_dir: "./test_tmp/persistence",
-  bin_ip: "false"
+  persistence_dir: "./test_tmp/persistence"
