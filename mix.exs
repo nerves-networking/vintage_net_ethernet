45f256c13bc71b15c5d427bc435d08e8c9e158c8
Frank Hunleth <fhunleth@troodon-software.com>
2021-08-22 16:05:43 -0400

    vintage_net: support ~> 0.11.0

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule VintageNetEthernet.MixProject do
 
   defp deps do
     [
-      {:vintage_net, "~> 0.10.0"},
+      {:vintage_net, "~> 0.10.0 or ~> 0.11.0"},
       {:credo, "~> 1.2", only: :test, runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false},
