000ecf0d42b222256d3c8661ac0b1c0f4f82cbae
Frank Hunleth <fhunleth@troodon-software.com>
2026-01-22 21:49:00 -0500

    Add assets to docs

--- mix.exs
+++ mix.exs
@@ -69,6 +69,7 @@ defmodule VintageNetEthernet.MixProject do
 
   defp docs do
     [
+      assets: %{"assets" => "assets"},
       extras: ["README.md", "CHANGELOG.md"],
       main: "readme",
       source_ref: "v#{@version}",
